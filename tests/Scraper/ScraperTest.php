7a8f03fc0ae726b52bd24b67627000769f015e21
Sofy Korzhova <sofykorzhova@gmail.com>
2022-09-19 15:21:19 +0200

    Added cache

--- tests/Scraper/ScraperTest.php
+++ tests/Scraper/ScraperTest.php
@@ -3,7 +3,7 @@
 namespace Sofyco\Spider\Tests\Scraper;
 
 use PHPUnit\Framework\TestCase;
-use Sofyco\Spider\ContextInterface;
+use Sofyco\Spider\Context;
 use Sofyco\Spider\Loader\LoaderInterface;
 use Sofyco\Spider\Scraper\Scraper;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
@@ -15,15 +15,11 @@ final class ScraperTest extends TestCase
         $url = 'https://localhost/example';
         $content = '<html>...</html>';
 
-        $context = $this->createMock(ContextInterface::class);
-        $context->expects($this->any())->method('getUrl')->willReturn($url);
-        $context->expects($this->any())->method('getExpiresAfter')->willReturn(100);
-
         $loader = $this->createMock(LoaderInterface::class);
         $loader->expects($this->any())->method('getContent')->willReturn($content);
 
         $cache = new ArrayAdapter();
-
+        $context = new Context(url: $url);
         $scraper = new Scraper($loader, $cache);
 
         self::assertSame($content, $scraper->getResult($context));

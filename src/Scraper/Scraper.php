6eabcf5f5304a532508eb968719f14a3638d93cc
Dmytro Khaperets <khaperets@gmail.com>
2025-03-01 01:39:12 +0100

    [Core] Optimize Scraper Performance

--- src/Scraper/Scraper.php
+++ src/Scraper/Scraper.php
@@ -3,16 +3,24 @@
 namespace Sofyco\Spider\Scraper;
 
 use Sofyco\Spider\ContextInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final readonly class Scraper implements ScraperInterface
 {
-    public function __construct(private HttpClientInterface $httpClient)
-    {
-    }
-
     public function getResult(ContextInterface $context): string
     {
-        return $this->httpClient->request(method: 'GET', url: $context->getUrl())->getContent();
+        $handle = curl_init(url: $context->getUrl());
+
+        curl_setopt(handle: $handle, option: CURLOPT_RETURNTRANSFER, value: true);
+        curl_setopt(handle: $handle, option: CURLOPT_FOLLOWLOCATION, value: true);
+
+        $response = curl_exec(handle: $handle);
+        $error = curl_error(handle: $handle);
+        curl_close(handle: $handle);
+
+        if (false === is_string($response)) {
+            throw new \RuntimeException(message: 'cURL error: ' . $error);
+        }
+
+        return $response;
     }
 }
